5393b03f0f7f773e647e37ba8350572a27fad3b7
MickeyJ <develop.agility@gmail.com>
2016-11-15 07:24:56 -0700

    Initial Commit

---
+++ SideMenuNib/ViewController4.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController4.swift
+//  SideMenuNib
+//
+//  Created by Michael Malotte on 11/15/16.
+//  Copyright © 2016 Michael Malotte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController4: UIViewController {
+  
+  @IBOutlet weak var menuButton: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      if revealViewController() != nil {
+        
+        menuButton.target = self.revealViewController()
+        menuButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+        
+        self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
+        
+      }
+
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+      
+      
+      
+    }
+  
+
+}
